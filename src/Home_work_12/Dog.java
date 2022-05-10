ab588b757baabbd2be331732aa190dde06e62be9
Biovttttt <biovttttt@gmail.com>
2022-05-10 13:54:25 +0600

    Добавлено методы SETS для класса CAT / и добавлено getter setter и метод toString

--- src/Home_work_12/Dog.java
+++ src/Home_work_12/Dog.java
@@ -1,4 +1,50 @@
 package Home_work_12;
-
+// Собака
 public class Dog {
+    private String breed ;// порода
+    private String name;
+    private String color;
+    private int age;
+
+
+    public String toString() {
+        return "Собака{" +
+                "Порода='" + breed + '\'' +
+                ", Имя='" + name + '\'' +
+                ", цвет='" + color + '\'' +
+                ", возраст=" + age +
+                '}';
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
--- src/Home_work_12/Main.java
+++ src/Home_work_12/Main.java
@@ -1,4 +1,71 @@
 package Home_work_12;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+        Parrot parrot = new Parrot();
+        // Кошка
+        cat.sets("Домашняя кошка","Кошка1","Белая",2);    // По методу SETS присваение сразу 4 переменных из класса CAT
+        // по сеттеру
+        cat.setBreed("Домашняя кошка");
+        cat.setName("Koшка");
+        cat.setColor("Белая");
+        cat.setAge(1);
+        // Собака // по сеттеру
+        dog.setBreed("Бульдог");
+        dog.setName("Алабай");
+        dog.setColor("Желтая");
+        dog.setAge(2);
+        // Рыба // по сеттеру
+        fish.setClas("хрящевых");
+        fish.setColor("Темно синая");
+        fish.setAge(1);
+        // Попугай // по сеттеру
+        parrot.setBreed("Корелла");
+        parrot.setName("Виктория");
+        parrot.setColor("Оранжевый");
+        parrot.setAge(2);
+        // Метод to String
+        System.out.println("+++++++++++++++++++++++++++++++по методу to string +++++++++++++++++++++++++++++++++++++++++++");
+        System.out.println(cat.toString());
+        System.out.println(dog.toString());
+        System.out.println(fish.toString());
+        System.out.println(parrot.toString());
+        // по Getter из класса Cat
+        System.out.println("++++++++++++++++++++++++++++++++++по Getter из класса Cat++++++++++++++++++++++++++++++++++++");
+        System.out.println("Кошка {" +
+                "Порода='" + cat.getBreed() + '\'' +
+                ", имя='" + cat.getName() + '\'' +
+                ", Цвет='" + cat.getColor() + '\'' +
+                ", Возраст=" + cat.getAge() +
+                '}');
+        // по Getter из класса Dog
+        System.out.println("+++++++++++++++++++++++++++++++++++// по Getter из класса Dog++++++++++++++++++++++++++++++++");
+        System.out.println("Собака {" +
+                "Порода='" + dog.getBreed() + '\'' +
+                ", имя='" + dog.getName() + '\'' +
+                ", Цвет='" + dog.getColor() + '\'' +
+                ", Возраст=" + dog.getAge() +
+                '}');
+        // по Getter из класса Parrot
+        System.out.println("+++++++++++++++++++++++++++++++++++++// по Getter из класса Parrot+++++++++++++++++++++++++++++++++++");
+        System.out.println("Попугай {" +
+                "Вид Попугая='" + parrot.getBreed() + '\'' +
+                ", имя='" + parrot.getName() + '\'' +
+                ", Цвет='" + parrot.getColor() + '\'' +
+                ", Возраст=" + parrot.getAge() +
+                '}');
+
+        System.out.println("+++++++++++++++++++++++++++++++++// по Getter из класса Fish+++++++++++++++++++++++++++++++++++++");
+        System.out.println("Рыба {" +
+                "Класс рыбы='" + fish.getClas() + '\'' +
+                ", Цвет='" + fish.getColor() + '\'' +
+                ", Возраст='" + fish.getAge() + '\'' +
+                 '}');
+    }
 }
