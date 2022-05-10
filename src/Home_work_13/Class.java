b625e9d8e7fcbe872755a373dbb0587028ce9436
Biovttttt <biovttttt@gmail.com>
2022-05-10 15:35:47 +0600

    Домашнее задание по 13 уроку, конструктор

---
+++ src/Home_work_13/Class.java
@@ -0,0 +1,25 @@
+package Home_work_13;
+
+public class Class {
+    private int number;
+    private String word;
+    private int massiv [];
+
+    Class(int number, String word, int[] massiv) {
+        this.number = number;
+        this.word = word;
+        this.massiv = massiv;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public int[] getMassiv() {
+        return massiv;
+    }
+}
