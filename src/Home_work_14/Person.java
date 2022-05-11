33080926d6fb5838ac2dd23d780f8836ac5e1ef6
Biovttttt <biovttttt@gmail.com>
2022-05-11 18:19:39 +0600

    Домашнее задание по 14 уроку, конструктор

---
+++ src/Home_work_14/Person.java
@@ -0,0 +1,17 @@
+package Home_work_14;
+
+public class Person {
+    private String name;
+    private String desingnation;
+
+    void learn(){
+
+    }
+    void walk(){
+
+    }
+    void eat(){
+
+    }
+
+}
