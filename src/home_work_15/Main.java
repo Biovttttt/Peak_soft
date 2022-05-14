c2ca1467ff9e0d6f7f0d965c92f10ac3b1ee4ad9
Biovttttt <biovttttt@gmail.com>
2022-05-14 14:24:09 +0600

    добавлено вывод после разделение животных из Animal massiva

--- src/home_work_15/Main.java
+++ src/home_work_15/Main.java
@@ -2,13 +2,16 @@ package home_work_15;
 
 public class Main {
     public static void main(String[] args) {
+
         Animal[] massiv = new Animal[3];
-        Shark[] massiv_shark = new Shark[1];
-        Turtle[] massiv_turtle = new Turtle[1];
-        Eagle[] massiv_eagle = new Eagle[1];
         massiv[0] = new Shark();
         massiv[1] = new Turtle();
         massiv[2] = new Eagle();
+
+ ///Для разделение создано 3 массива ниже
+        Shark[] massiv_shark = new Shark[1];
+        Turtle[] massiv_turtle = new Turtle[1];
+        Eagle[] massiv_eagle = new Eagle[1];
 
         for (Animal a : massiv) {
 
