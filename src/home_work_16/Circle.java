43f85aff94c74622b60461cefcb4e512448eefa2
Biovttttt <biovttttt@gmail.com>
2022-05-14 14:43:00 +0600

    Домашнее задание 16

---
+++ src/home_work_16/Circle.java
@@ -0,0 +1,12 @@
+package home_work_16;
+
+public class Circle {
+    static double pi=Math.PI;
+
+    static void area (double radius){
+        System.out.println("area= "+ pi*radius*radius);
+    }
+    static  void circumference(double radius){
+        System.out.println("circumference = "+ pi*2*radius);
+    }
+}
