3fd96147bdedfd4e921181460ef189474e6915d4
Biovttttt <biovttttt@gmail.com>
2022-05-30 15:29:08 +0600

    Домашнее задание по 17 уроку 2 пункт

---
+++ src/home_work_17_2/A.java
@@ -0,0 +1,8 @@
+package home_work_17_2;
+
+public class A extends Shape {
+    @Override
+    public double getPerimeter() {
+        return 1;
+    }
+}
---
+++ src/home_work_17_2/C.java
@@ -0,0 +1,8 @@
+package home_work_17_2;
+
+public class C extends Shape {
+    @Override
+    public double getPerimeter() {
+        return 3;
+    }
+}
